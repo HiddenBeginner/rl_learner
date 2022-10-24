be3a5d7ca8fbefcb44e966bd045b4da0419401bf
HiddenBeginner <mostp______@naver.com>
2022-10-24 15:03:05 +0900

    Denoramlize a continuous action to the original action space

--- rl_learner/envs/__init__.py
+++ rl_learner/envs/__init__.py
@@ -2,6 +2,18 @@ import gym
 import numpy as np
 
 
+class DenormalizeContinuousAction(gym.Wrapper):
+    def __init__(self, env):
+        gym.Wrapper.__init__(self, env)
+
+    def step(self, action):
+        action = (0.5 * (action + 1)) * (
+            self.env.action_space.high - self.env.action_space.low
+        ) + self.env.action_space.low
+
+        return self.env.step(action)
+
+
 class ScalarActionToArrayEnv(gym.Wrapper):
     def __init__(self, env):
         gym.Wrapper.__init__(self, env)
@@ -15,6 +27,9 @@ class ScalarActionToArrayEnv(gym.Wrapper):
 
 def make(env_name):
     env = gym.make(env_name)
+
+    if isinstance(env.action_space, gym.spaces.Box):
+        env = DenormalizeContinuousAction(env)
 
     if len(env.action_space.shape) == 0:
         env = ScalarActionToArrayEnv(env)
